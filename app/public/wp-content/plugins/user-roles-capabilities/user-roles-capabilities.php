5cf334d13d2a1fcdbd42cb5d0b9c4ca1ae3e135e
KaptenAlex <alexander.m.wilson1996@gmail.com>
2020-02-28 12:24:46 +0100

    Added query monitor and plugin for changing role capabilities.

--- app/public/wp-content/plugins/user-roles-capabilities/user-roles-capabilities.php
+++ app/public/wp-content/plugins/user-roles-capabilities/user-roles-capabilities.php
@@ -4,15 +4,33 @@
  *  Plugin name:    User roles for real estate
  *  Description:    Plugin for managing capabilities for different WordPress roles.
  *  Author:         Alexander Wilson
- *  Version:        0.1
+ *  Version:        0.2
  *  Text Domain:    user-roles-capabilities
  */
 
+/*
+ *
+ ** Register activation hooks
+ *
+ */
 function alter_author_role() {
     global $wp_roles;
     $wp_roles->remove_cap( 'author', 'publish_posts' );
 }
 register_activation_hook( __FILE__, 'alter_author_role' );
+
+function alter_editor_role() {
+    global $wp_roles;
+    $wp_roles->remove_cap( 'editor', 'delete_published_posts' );
+
+}
+register_activation_hook( __FILE__, 'alter_editor_role' );
+
+/*
+ *
+ ** Register deactivation hooks
+ *
+ */
 
 function remove_author_alteration()
 {
@@ -20,3 +38,10 @@ function remove_author_alteration()
     $wp_roles->add_cap( 'author', 'publish_posts' );
 }
 register_deactivation_hook( __FILE__, 'remove_author_alteration' );
+
+function remove_editor_alteration()
+{
+    global $wp_roles;
+    $wp_roles->add_cap( 'editor', 'delete_published_pages' );
+}
+register_deactivation_hook( __FILE__, 'remove_editor_alteration' );
