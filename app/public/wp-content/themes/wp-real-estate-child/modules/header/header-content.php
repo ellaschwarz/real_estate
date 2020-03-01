aa96c6c5ce2c0a71efd1c08b638aaac222a267d2
KaptenAlex <alexander.m.wilson1996@gmail.com>
2020-03-01 19:06:13 +0100

    Started on carousel for front page selected items.

--- app/public/wp-content/themes/wp-real-estate-child/modules/header/header-content.php
+++ app/public/wp-content/themes/wp-real-estate-child/modules/header/header-content.php
@@ -1,7 +1,23 @@
 <?php if (has_header_image() & is_front_page()) : ?>
     <div id="header-image" class="container">
-        <img src="<?php header_image(); ?>" width="100%">
+<?php
+    $args = [
+        'post_type'	    => 'objects',
+        //'meta_key'		=> 'selected',
+        //'meta_value'	=> 'true',
+    ];
 
+    $query = new WP_Query( $args );
+    echo "HEJ";
+    if( $query->have_posts() ):
+       while( $query->have_posts() ) : $query->the_post(); ?>
+          <p><?php echo the_title(); ?></p>
+       <?php endwhile; ?>
+    <?php endif; ?>
+
+    <?php wp_reset_query();	 // Restore global post data stomped by the_post().
+
+ ?>
         <?php if (get_theme_mod('wpre_hinfo_enable') && (get_theme_mod('wpre_header_content_page') != '' )) :
             $pageid = get_theme_mod('wpre_header_content_page'); ?>
             <div class="header-information">
