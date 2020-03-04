98af3efee02720ab0cd151d012202bdcda92cc2f
Pat <sipapat.prasopsap@gmail.com>
2020-03-04 16:46:46 +0100

    Fixed footer

--- app/public/wp-content/plugins/brooker_widget/brooker_widget.php
+++ app/public/wp-content/plugins/brooker_widget/brooker_widget.php
@@ -72,55 +72,39 @@ class Realestate_Broker_Widget extends WP_Widget
         echo $args['after_widget'];
     }
 
-    /**
-     * Back-end widget form.
-     *
-     * @see WP_Widget::form()
-     *
-     * @param array $instance Previously saved values from database.
-     */
-    public function form($instance)
-    {
-        $title = !empty($instance['title']) ? $instance['title'] : esc_html__('New title', 'text_domain');
+	/**
+	 * Back-end widget form.
+	 *
+	 * @see WP_Widget::form()
+	 *
+	 * @param array $instance Previously saved values from database.
+	 */
+	public function form( $instance ) {
+		$title = ! empty( $instance['title'] ) ? $instance['title'] : esc_html__( 'New title', 'text_domain' );
+		?>
+		<p>
+		<label for="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>"><?php esc_attr_e( 'Title:', 'text_domain' ); ?></label> 
+		<input class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'title' ) ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>">
+		</p>
+		<?php 
+	}
 
-        $quantity = !empty($instance['quantity']) ? $instance['quantity'] : esc_html__('1', 'text_domain');
-        ?>
-<p>
-    <label
-        for="<?php echo esc_attr($this->get_field_id('title')); ?>"><?php esc_attr_e('Title:', 'text_domain');?></label>
-    <input class="widefat" id="<?php echo esc_attr($this->get_field_id('title')); ?>"
-        name="<?php echo esc_attr($this->get_field_name('title')); ?>" type="text"
-        value="<?php echo esc_attr($title); ?>">
-</p>
+	/**
+	 * Sanitize widget form values as they are saved.
+	 *
+	 * @see WP_Widget::update()
+	 *
+	 * @param array $new_instance Values just sent to be saved.
+	 * @param array $old_instance Previously saved values from database.
+	 *
+	 * @return array Updated safe values to be saved.
+	 */
+	public function update( $new_instance, $old_instance ) {
+		$instance = array();
+		$instance['title'] = ( ! empty( $new_instance['title'] ) ) ? sanitize_text_field( $new_instance['title'] ) : '';
 
-<p>
-    <label
-        for="<?php echo esc_attr($this->get_field_id('quantity')); ?>"><?php esc_attr_e('Amount of broker to Display   :', 'text_domain');?></label>
-    <input class="widefat" id="<?php echo esc_attr($this->get_field_id('quantity')); ?>"
-        name="<?php echo esc_attr($this->get_field_name('title')); ?>" type="number"
-        value="<?php echo esc_attr($quantity); ?>" min='0'>
-</p>
-<?php
-}
-
-    /**
-     * Sanitize widget form values as they are saved.
-     *
-     * @see WP_Widget::update()
-     *
-     * @param array $new_instance Values just sent to be saved.
-     * @param array $old_instance Previously saved values from database.
-     *
-     * @return array Updated safe values to be saved.
-     */
-    public function update($new_instance, $old_instance)
-    {
-        $instance = array();
-        $instance['title'] = (!empty($new_instance['title'])) ? sanitize_text_field($new_instance['title']) : '';
-
-        $instance['quantity'] = (!empty($new_instance['quantity'])) ? sanitize_text_field($new_instance['quantity']) : '';
-        return $instance;
-    }
+		return $instance;
+	}
 
 } // class Foo_Widget
 // register Foo_Widget widget
