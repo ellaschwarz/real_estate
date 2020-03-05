ee4cc619782a596257844b4d107090cf6927bc51
KaptenAlex <alexander.m.wilson1996@gmail.com>
2020-03-05 16:03:52 +0100

    Changed posts_per_page to 5 to meet demands.

--- app/public/wp-content/themes/wp-real-estate-child/functions.php
+++ app/public/wp-content/themes/wp-real-estate-child/functions.php
@@ -69,7 +69,7 @@ function homepage_main_query( $query ) {
 
     //Check if the query is performed on the homepage
     if(is_home()){
-      $query->set('posts_per_page', 6);
+      $query->set('posts_per_page', 5);
       $query->set('post_type', 'objects');
     }
   }
