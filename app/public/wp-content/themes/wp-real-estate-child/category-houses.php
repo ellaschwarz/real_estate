a302e440df409f7c1e3f30061dea30d188463680
KaptenAlex <alexander.m.wilson1996@gmail.com>
2020-03-05 16:08:04 +0100

    Removed sidebar in category-houses.php to make it look like the other category-pages

--- app/public/wp-content/themes/wp-real-estate-child/category-houses.php
+++ app/public/wp-content/themes/wp-real-estate-child/category-houses.php
@@ -25,20 +25,20 @@ $args = [
  $object = new WP_query($args);
 
 if ($object ->have_posts()) : ?>
-         
+
           <?php while ($object ->have_posts()) :
                 $object ->the_post(); ?>
-                         <div class="selected_post"> 
+                         <div class="selected_post">
                               <div class="carousel_category">
                               <a href="<?php the_permalink(); ?>"><?php the_content(); ?></a>
                          </div>
-                         <div class="entry">	
+                         <div class="entry">
                                    <div class="title_area_city">
                                    <h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
-                                   <h4><i class="fas fa-home"></i><?php echo get_post_meta(get_the_ID(), 'area', true); ?> , 
+                                   <h4><i class="fas fa-home"></i><?php echo get_post_meta(get_the_ID(), 'area', true); ?> ,
                                         <?php echo get_post_meta(get_the_ID(), 'city', true); ?> </h4>
                                         </div>
-                                        
+
                                         <h4 class="price">Price: <?php echo get_post_meta(get_the_ID(), 'price', true); ?> kr</h4>
                                         <h4 class="m2"> m²: <?php echo get_post_meta(get_the_ID(), 'm²', true); ?></h4>
                                         <h4 class="rooms"> Rooms: <?php echo get_post_meta(get_the_ID(), 'rooms', true); ?></h4>
@@ -70,13 +70,13 @@ if ($object ->have_posts()) : ?>
      <?php while ($object ->have_posts()) :
             $object ->the_post(); ?>
 
-            <div class="post"> 
+            <div class="post">
                  <div class='photo_category'>
                  <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail(array(512, 340)); ?></a>
                </div>
-               <div class="entry_post">                            
+               <div class="entry_post">
                     <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-                    <h4><?php echo get_post_meta(get_the_ID(), 'area', true); ?> , 
+                    <h4><?php echo get_post_meta(get_the_ID(), 'area', true); ?> ,
                                 <?php echo get_post_meta(get_the_ID(), 'city', true); ?> </h4>
                     <h4>Price: <?php echo get_post_meta(get_the_ID(), 'price', true); ?> kr</h4>
                     <h4> m²: <?php echo get_post_meta(get_the_ID(), 'm²', true); ?></h4>
@@ -91,6 +91,5 @@ if ($object ->have_posts()) : ?>
 endif; ?>
 
 
-<?php get_sidebar(); ?>
 <div style = "clear:both"></div>	
 <?php get_footer(); ?>
